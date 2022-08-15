fd0abf84a4d58b98fac6b7f2297e3ca44530123b
Caique Siqueira <latorrecaique@gmail.com>
2022-08-15 17:53:05 -0300

    AB#69962 Endpoint de totais

---
+++ src/SME.SERAp.Prova.Acompanhamento.Dados/Interfaces/IRepositorioTotais.cs
@@ -0,0 +1,14 @@
+﻿using SME.SERAp.Prova.Acompanhamento.Infra.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SME.SERAp.Prova.Acompanhamento.Dados.Interfaces
+{
+    public interface IRepositorioTotais 
+    {
+       
+    }
+}
