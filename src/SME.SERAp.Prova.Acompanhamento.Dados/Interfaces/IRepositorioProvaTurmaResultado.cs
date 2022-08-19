2267aacc2995dd7d9da889120f19b7bff64fa108
Juliano Martins <juliano_campos_92@hotmail.com>
2022-08-19 09:34:15 -0400

    totalizadores do incide provaTurmaResultado

--- src/SME.SERAp.Prova.Acompanhamento.Dados/Interfaces/IRepositorioProvaTurmaResultado.cs
+++ src/SME.SERAp.Prova.Acompanhamento.Dados/Interfaces/IRepositorioProvaTurmaResultado.cs
@@ -1,4 +1,5 @@
 ﻿using SME.SERAp.Prova.Acompanhamento.Dominio.Entities;
+using SME.SERAp.Prova.Acompanhamento.Infra.Dtos;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -7,5 +8,9 @@ namespace SME.SERAp.Prova.Acompanhamento.Dados.Interfaces
     public interface IRepositorioProvaTurmaResultado : IRepositorioBase<ProvaTurmaResultado>
     {
         Task<IEnumerable<ProvaTurmaResultado>> ObterResumoGeralPorFiltroAsync(long provaId, int? dreId, int? ueId, string anoEscolar, long? turmaId);
+        Task<double> ObterTotalProvasFinalizadasPorFiltroAsync(FiltroDto filtro);
+        Task<double> ObterTotalProvasIniciadasHojePorFiltroAsync(FiltroDto filtro);
+        Task<double> ObterTotalProvasNaoFinalizadasPorFiltroAsync(FiltroDto filtro);
+        Task<double> ObterTotalProvasPorFiltroAsync(FiltroDto filtro);
     }
 }
