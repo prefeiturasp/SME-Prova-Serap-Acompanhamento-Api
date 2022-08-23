400da810e7a361fbb85d96bfbd3d6177579c85d1
Juliano Martins <juliano_campos_92@hotmail.com>
2022-08-23 19:55:50 -0400

    correção

--- src/SME.SERAp.Prova.Acompanhamento.Infra/Dtos/DetalheProvaDto.cs
+++ src/SME.SERAp.Prova.Acompanhamento.Infra/Dtos/DetalheProvaDto.cs
@@ -11,10 +11,18 @@ namespace SME.SERAp.Prova.Acompanhamento.Infra
 
         public DateTime DataInicio { get; set; }
         public DateTime DataFim { get; set; }
-        public int QtdeQuestoesProva { get; set; }
+        public long QtdeQuestoesProva { get; set; }
         public decimal TotalQuestoes { get; set; }
         public decimal Respondidas { get; set; }
-        public decimal PercentualRespondido { get; set; }
-
+        public decimal PercentualRespondido
+        {
+            get
+            {
+                if (TotalQuestoes == 0 || Respondidas == 0) return 0;
+                var percentual = (Respondidas * 100) / TotalQuestoes;
+                percentual = Convert.ToDecimal(percentual);
+                return percentual > 100 ? 100 : Math.Round(percentual, 2);
+            }
+        }
     }
 }
