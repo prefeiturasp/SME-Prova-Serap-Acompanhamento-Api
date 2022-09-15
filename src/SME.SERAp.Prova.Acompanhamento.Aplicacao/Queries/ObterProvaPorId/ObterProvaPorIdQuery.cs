ee156864357fb48a34c6202677fbf430ee36f27f
Caique Siqueira <latorrecaique@gmail.com>
2022-09-15 17:07:06 -0300

    AB#74016 AB#74288 Criar verificação se a prova pode ou não ser reaberta

---
+++ src/SME.SERAp.Prova.Acompanhamento.Aplicacao/Queries/ObterProvaPorId/ObterProvaPorIdQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SME.SERAp.Prova.Acompanhamento.Aplicacao
+{
+    public class ObterProvaPorIdQuery : IRequest<Dominio.Entities.Prova>
+    {
+        public ObterProvaPorIdQuery(long provaId)
+        {
+            ProvaId = provaId;
+        }
+
+        public long ProvaId { get; set; }
+    }
+}
