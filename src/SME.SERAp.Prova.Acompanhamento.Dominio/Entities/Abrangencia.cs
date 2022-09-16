b5dab1e088961aa8c4e51a1c4fc3a61d9b1e3581
Juliano Martins <juliano_campos_92@hotmail.com>
2022-09-16 17:44:06 -0400

    correção

--- src/SME.SERAp.Prova.Acompanhamento.Dominio/Entities/Abrangencia.cs
+++ src/SME.SERAp.Prova.Acompanhamento.Dominio/Entities/Abrangencia.cs
@@ -6,7 +6,7 @@ namespace SME.SERAp.Prova.Acompanhamento.Dominio.Entities
     {
         public Abrangencia() { }
 
-        public Abrangencia(Guid usuarioId, string login, string usuario, Guid grupoId, string grupo, bool permiteConsultar, bool permiteAlterar, long dreId, long ueId, long turmaId)
+        public Abrangencia(string usuarioId, string login, string usuario, string grupoId, string grupo, bool permiteConsultar, bool permiteAlterar, long dreId, long ueId, long turmaId)
         {
             UsuarioId = usuarioId;
             Login = login;
@@ -22,10 +22,10 @@ namespace SME.SERAp.Prova.Acompanhamento.Dominio.Entities
             Id = $"{GrupoId}-{UsuarioId}-{DreId}-{UeId}-{TurmaId}";
         }
 
-        public Guid UsuarioId { get; set; }
+        public string UsuarioId { get; set; }
         public string Login { get; set; }
         public string Usuario { get; set; }
-        public Guid GrupoId { get; set; }
+        public string GrupoId { get; set; }
         public string Grupo { get; set; }
         public bool PermiteConsultar { get; set; }
         public bool PermiteAlterar { get; set; }
