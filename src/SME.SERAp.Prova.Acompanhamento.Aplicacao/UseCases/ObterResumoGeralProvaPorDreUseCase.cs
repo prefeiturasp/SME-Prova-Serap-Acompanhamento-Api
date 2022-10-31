b1486c7393a98edad0737aba62aadb7f921f8974
Caique Siqueira <latorrecaique@gmail.com>
2022-10-31 11:23:50 -0300

    AB#78270

--- src/SME.SERAp.Prova.Acompanhamento.Aplicacao/UseCases/ObterResumoGeralProvaPorDreUseCase.cs
+++ src/SME.SERAp.Prova.Acompanhamento.Aplicacao/UseCases/ObterResumoGeralProvaPorDreUseCase.cs
@@ -41,7 +41,7 @@ namespace SME.SERAp.Prova.Acompanhamento.Aplicacao.UseCases
             resumoGeral.TotalPaginas = (int)Math.Ceiling((double)resumoGeral.TotalRegistros / filtro.NumeroRegistros);
 
             var skip = (filtro.NumeroPagina - 1) * filtro.NumeroRegistros;
-            resumoGeral.Items = listaResumoGeralDre.Skip(skip).Take(filtro.NumeroRegistros).ToList();
+            resumoGeral.Items = listaResumoGeralDre.Skip(skip).Take(filtro.NumeroRegistros).OrderBy(x=> x.Nome).ToList();
 
             return resumoGeral;
 
