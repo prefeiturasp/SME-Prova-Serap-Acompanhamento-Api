6e96793bac96a6c408a6313f34867108b058c2ce
Caique Siqueira <latorrecaique@gmail.com>
2022-11-03 19:31:13 -0300

    logAutenticacao

--- src/SME.SERAp.Prova.Acompanhamento.Aplicacao/UseCases/AutenticacaoUseCase.cs
+++ src/SME.SERAp.Prova.Acompanhamento.Aplicacao/UseCases/AutenticacaoUseCase.cs
@@ -2,6 +2,7 @@
 using SME.SERAp.Prova.Acompanhamento.Aplicacao.Interfaces;
 using SME.SERAp.Prova.Acompanhamento.Infra.Dtos;
 using SME.SERAp.Prova.Acompanhamento.Infra.Exceptions;
+using SME.SERAp.Prova.Acompanhamento.Infra.Interfaces;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace SME.SERAp.Prova.Acompanhamento.Aplicacao.UseCases
 {
     public class AutenticacaoUseCase : AbstractUseCase, IAutenticacaoUseCase
     {
+         
         public AutenticacaoUseCase(IMediator mediator) : base(mediator)
         {
         }
@@ -18,7 +20,7 @@ namespace SME.SERAp.Prova.Acompanhamento.Aplicacao.UseCases
             var abrangencias = await mediator.Send(new ObterAbrangenciaPorLoginGrupoQuery(autenticacaoDto.Login, autenticacaoDto.Perfil));
 
             if (abrangencias == null || !abrangencias.Any())
-                throw new NaoAutorizadoException("Usuário inválido", 401);
+                throw new NaoAutorizadoException($"Usuário: {autenticacaoDto.Login} inválido com o grupo {autenticacaoDto.Perfil}.", 401);
 
             return await mediator.Send(new GerarCodigoValidacaoAutenticacaoCommand(abrangencias));
         }
